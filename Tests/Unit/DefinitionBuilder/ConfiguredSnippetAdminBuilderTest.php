502e03e20375eac74f7acb183c93087d5e063cd3
MarkusHolstein <mh@perspeqtive.de>
2025-06-16 12:04:42 +0200

    Test ConfiguredSnippetAdminBuilder

---
+++ Tests/Unit/DefinitionBuilder/ConfiguredSnippetAdminBuilderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PERSPEQTIVE\SuluSnippetManagerBundle\Tests\Unit\DefinitionBuilder;
+
+use PERSPEQTIVE\SuluSnippetManagerBundle\Admin\ConfiguredParentMenuAdmin;
+use PERSPEQTIVE\SuluSnippetManagerBundle\Admin\ConfiguredSnippetAdmin;
+use PERSPEQTIVE\SuluSnippetManagerBundle\DefinitionBuilder\ConfiguredParentDefinitionBuilder;
+use PERSPEQTIVE\SuluSnippetManagerBundle\DefinitionBuilder\ConfiguredSnippetAdminBuilder;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class ConfiguredSnippetAdminBuilderTest extends TestCase
+{
+    public function testGenerate(): void {
+        $baseDefinition = new Definition(ConfiguredSnippetAdmin::class, ['testargument']);
+        $baseDefinition->setAbstract(true);
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->addDefinitions([
+            'perspeqtive_sulu_snippet_manager.admin.configured_snippet_admin' =>
+                $baseDefinition
+        ]);
+        $definitionBuilder = new ConfiguredSnippetAdminBuilder();
+        $definition = $definitionBuilder->build([ 'navigation_title' => 'Test', 'type' => 'shop','order' => 10, 'icon' => 'su-icon'], $containerBuilder, 'testparent');
+
+        self::assertTrue($baseDefinition->isAbstract());
+        self::assertFalse($definition->isAbstract());
+
+        self::assertSame(ConfiguredSnippetAdmin::class, $definition->getClass());
+        self::assertSame([
+            'testargument',
+            'shop',
+            'Test',
+            10,
+            'su-icon',
+            'testparent'
+        ],
+            $definition->getArguments());
+    }
+
+}
